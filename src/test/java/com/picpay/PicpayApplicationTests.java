5f8cb612e405f7e1e7639d2b58e2ff338fa9f10e
D3vN0t <d3vn0t@proton.me>
2023-08-19 08:21:26 -0300

    feat: initial config

---
+++ src/test/java/com/picpay/PicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
